3e8ceeb865efba3613c01c37a99106deb60d5718
Khusaeriz <akhus22@gmail.com>
2019-04-02 18:56:58 +0700

    initial commit

---
+++ content.php
@@ -0,0 +1,80 @@
+
+        <div class="row">
+          <div class="col-lg-12">
+              <div class="box">
+                  <div class="box-header with-border">
+                    <h3 class="box-title">Table Data Obat</h3>
+                    <hr/>
+                    <div>
+                      <button class="btn btn-primary"><i class="fa fa-plus"></i> Tambah</button>
+                      <button class="btn btn-warning"><i class="fa fa-edit"></i> Edit</button>
+                      <button class="btn btn-primary"><i class="fa fa-print"></i> Cetak</button>
+                      <button class="btn btn-danger"><i class="fa fa-trash"></i> Hapus</button>
+                    </div>
+                  </div>
+                  <!-- /.box-header -->
+                  <div class="box-body">
+                    <table class="table table-bordered">
+                      <tbody><tr>
+                        <th style="width: 10px"></th>
+                        <th>Kd. Obat</th>
+                        <th>Nama Obat</th>
+                        <th>Harga</th>
+                        <th>Tgl Expired</th>
+                        <th>Stok</th>
+                      </tr>
+                      <tr>
+                        <td><input type="radio" name="id" id=""></td>
+                        <td>O-001</td>
+                        <td>Amitanol</td>
+                        <td>
+                          Rp. 30.000,00
+                        </td>
+                        <td>22/01/2020</td>
+                        <td>20</td>
+                      </tr>
+                      <tr>
+                        <td><input type="radio" name="id" id=""></td>
+                        <td>O-002</td>
+                        <td>Parasetamol</td>
+                        <td>
+                            Rp. 30.000,00
+                        </td>
+                        <td>22/01/2020</td>
+                        <td>14</td>
+                      </tr>
+                      <tr>
+                        <td><input type="radio" name="id" id=""></td>
+                        <td>O-003</td>
+                        <td>Ethanol</td>
+                        <td>
+                            Rp. 30.000,00
+                        </td>
+                        <td>22/01/2020</td>
+                        <td>10</td>
+                      </tr>
+                      <tr>
+                        <td><input type="radio" name="id" id=""></td>
+                        <td>O-004</td>
+                        <td>Alcohol</td>
+                        <td>
+                            Rp. 30.000,00
+                        </td>
+                        <td>22/01/2020</td>
+                        <td>7</td>
+                      </tr>
+                    </tbody></table>
+                  </div>
+                  <!-- /.box-body -->
+                  <div class="box-footer clearfix">
+                    <ul class="pagination pagination-sm no-margin pull-right">
+                      <li><a href="#">«</a></li>
+                      <li><a href="#">1</a></li>
+                      <li><a href="#">2</a></li>
+                      <li><a href="#">3</a></li>
+                      <li><a href="#">»</a></li>
+                    </ul>
+                  </div>
+                </div>
+          </div>
+        </div>
